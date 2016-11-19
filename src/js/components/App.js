b1cd80f2c33edb8d222e7ba1c703061a51051ebe
perohrstrom <per.k.ohrstrom@gmail.com>
2016-11-19 18:16:40 -0500

    remove  MovieForm from app

--- src/js/components/App.js
+++ src/js/components/App.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import Materialize from 'materialize-css';
 import Episode from './Episode';
-import MovieForm from './MovieForm';
 import NavBar from './NavBar';
 import guests from '../../data/guests';
 import Footer from './Footer';
